56f63ac94619c8f253ef4ac615890b43f13014be
EvgeniyFerz <inbox.konovalov@gmail.com>
2025-04-07 23:13:34 +0300

    Delete callbacks.py

--- callbacks.py
+++
@@ -1,5 +0,0 @@
-from telegram import Update
-from telegram.ext import ContextTypes
-
-async def button_handler(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    await update.callback_query.answer("Кнопка нажата!")
