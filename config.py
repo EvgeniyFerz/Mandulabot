39e5369189afbb7c7549b9d333262efd4579f999
EvgeniyFerz <inbox.konovalov@gmail.com>
2025-04-07 22:38:45 +0300

    Update config.py

--- config.py
+++ config.py
@@ -4,8 +4,8 @@ from dotenv import load_dotenv
 # Загружаем переменные из .env
 load_dotenv()
 
-# Ваш токен бота
-TOKEN = os.getenv('TELEGRAM_BOT_TOKEN')
+# Токен вашего Telegram-бота
+TELEGRAM_BOT_TOKEN = os.getenv('TELEGRAM_BOT_TOKEN')
 
 # URL для keep-alive
 WEBHOOK_HOST = os.getenv('WEBHOOK_HOST')
